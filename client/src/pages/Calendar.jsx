eb3dc6fddc7c4b9d4aeda3f00665bffda1f4c28a
G'ulomjon Bakirov <80908091+PandaCoderSudo@users.noreply.github.com>
2021-09-11 20:16:28 -0600

    Some Fixes in calendar page

--- client/src/pages/Calendar.jsx
+++ client/src/pages/Calendar.jsx
@@ -1,7 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import DatePicker from "react-datepicker";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-datepicker/dist/react-datepicker.css";
+import { Modal, Button } from "antd";
 
-const Calendar = () => {
-  return <div>Calendar</div>;
+const locales = {
+  "en-US": require("date-fns/locale/en-US"),
 };
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
 
-export default Calendar;
+const events = [
+  {
+    title: "Big Meeting",
+    allDay: true,
+    start: new Date(2021, 6, 0),
+    end: new Date(2021, 6, 0),
+  },
+  {
+    title: "Vacation",
+    start: new Date(2021, 6, 7),
+    end: new Date(2021, 6, 10),
+  },
+  {
+    title: "Conference",
+    start: new Date(2021, 6, 20),
+    end: new Date(2021, 6, 23),
+  },
+];
+
+function AdminCalendar() {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  const [allEvents, setAllEvents] = useState(events);
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  function handleAddEvent() {}
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setAllEvents([...allEvents, newEvent]);
+    setIsModalVisible(false);
+    setNewEvent({ title: "", start: "", end: "" });
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <div className="AdminCalendar">
+      <h1>Calendar</h1>
+      <div>
+        <Button type="primary" onClick={showModal}>
+          Add +
+        </Button>
+        <Modal
+          title="Basic Modal"
+          visible={isModalVisible}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <input
+            type="text"
+            placeholder="Add Title"
+            style={{ width: "20%", marginRight: "10px" }}
+            value={newEvent.title}
+            onChange={(e) =>
+              setNewEvent({ ...newEvent, title: e.target.value })
+            }
+          />
+          <DatePicker
+            placeholderText="Start Date"
+            style={{ marginRight: "10px" }}
+            selected={newEvent.start}
+            onChange={(start) => setNewEvent({ ...newEvent, start })}
+          />
+          <DatePicker
+            placeholderText="End Date"
+            selected={newEvent.end}
+            onChange={(end) => setNewEvent({ ...newEvent, end })}
+          />
+        </Modal>
+      </div>
+      <Calendar
+        localizer={localizer}
+        events={allEvents}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500, margin: "0px" }}
+      />
+    </div>
+  );
+}
+
+export default AdminCalendar;
